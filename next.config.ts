02720bb88be93032db789383d70ed2b2d3ea90fc
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-04-22 12:04:28 -0500

    Refactor Next.js configuration for image optimization and clean up comments

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Autorizamos a Next.js a optimizar imágenes de Unsplash */
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
         port: "",
-        pathname: "/**", // Permite cualquier ruta dentro de este dominio
+        pathname: "/**",
       },
     ],
   },
-  /* Mantenemos la redirección automática al idioma italiano */
+
   async redirects() {
     return [
       {
